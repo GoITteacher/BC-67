64519beccd266645bf1b9dbe6b4df676d1df23ca
GO IT Teacher <50087029+GoITteacher@users.noreply.github.com>
2023-12-15 08:49:59 +0200

    update

--- js/01-coercion.js
+++ js/01-coercion.js
@@ -5,7 +5,7 @@
  * - Неявне перетворення типів
  */
 
-console.log(Number("5")); // 5
+/* console.log(Number("5")); // 5
 console.log(Number(true)); // 1
 console.log(Number(false)); // 0
 console.log(Number(null)); // 0
@@ -15,4 +15,4 @@ console.log(Number("25px")); // NaN
 console.log("5" * 2); // 10
 console.log("10" - 5); // 5
 console.log(5 + true); // 6
-console.log(5 - true); // 4
+console.log(5 - true); // 4 */
