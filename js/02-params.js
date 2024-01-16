c3a5776398bd3abcb17218d4ffba7591fd1d950c
GO IT Teacher <50087029+GoITteacher@users.noreply.github.com>
2024-01-16 12:05:19 +0200

    update

--- js/02-params.js
+++ js/02-params.js
@@ -1,9 +1,39 @@
+/* function foo({
+  firstname,
+  lastname,
+  patronymic,
+  age,
+  money,
+  isStudent,
+  isAdmin,
+}) {
+  console.log(firstname);
+  console.log(lastname);
+  console.log(patronymic);
+  console.log(age);
+  console.log(money);
+  console.log(isStudent);
+  console.log(isAdmin);
+}
+
+const obj = {
+  money: 40,
+  firstname: 'awdawd',
+  isAdmin: true,
+  patronymic: 'sefsef',
+  age: 20,
+  isStudent: false,
+  lastname: 'adaw',
+};
+
+foo(obj); */
+
 /**
  * Деструктуризація об'єкта в параметрах функції
  */
 
 const user = {
-  username: "Jacob name",
+  username: 'Jacob name',
   skills: {
     html: true,
     css: false,
@@ -12,12 +42,33 @@ const user = {
 };
 
 // Без деструктуризації
-function getUserName(obj) {
+
+/* function getUserName(obj) {
   console.log(
-    `Hello my name is ${obj.name}, I know html - ${obj.skills.html}, css - ${obj.skills.css} and js -${obj.skills.js}`
+    `Hello my name is ${obj.name}, I know html - ${obj.skills.html}, css - ${obj.skills.css} and js -${obj.skills.js}`,
   );
-}
+} */
 
 // З деструктуризацією
 
-getUserName(user);
+/* function getUserName({ username, skills: { html, css, js } }) {
+  console.log(
+    `Hello my name is ${username}, I know html - ${html}, css - ${css} and js -${js}`,
+  );
+} */
+
+/* function foo({
+  id = 'test_id',
+  title = 'test_title',
+  author = 'test_author',
+} = {}) {
+  console.log(id, title, author);
+}
+
+const book = {
+  id: '123123',
+  author: 'awdawd',
+  desc: 'drgd',
+};
+
+foo(book); */
