dc3c6e429f7d28e279e23bb54e2ed6e83473513f
GoITteacher <50087029+GoITteacher@users.noreply.github.com>
2024-01-18 10:56:02 +0000

    Deploying to gh-pages from @ GoITteacher/BC-67@bb63e6bb736dc86492861fbc26fc4538db878817 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,11 +1,15 @@
-(function(){const s=document.createElement("link").relList;if(s&&s.supports&&s.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))o(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const i of t.addedNodes)i.tagName==="LINK"&&i.rel==="modulepreload"&&o(i)}).observe(document,{childList:!0,subtree:!0});function n(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function o(e){if(e.ep)return;e.ep=!0;const t=n(e);fetch(e.href,t)}})();class a{constructor(s,n,o){this.username=s,this.email=n,this.text=o}toString(){return`<li class="box message">
+import{v as c,b as l}from"./assets/vendor-2a142fa9.js";(function(){const r=document.createElement("link").relList;if(r&&r.supports&&r.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))o(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const i of t.addedNodes)i.tagName==="LINK"&&i.rel==="modulepreload"&&o(i)}).observe(document,{childList:!0,subtree:!0});function n(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function o(e){if(e.ep)return;e.ep=!0;const t=n(e);fetch(e.href,t)}})();class m{constructor(r,n,o){this.id=c(),this.username=r,this.email=n,this.text=o}toString(){return`<li class="box message" data-id="${this.id}">
     <h3>${this.username} - ${this.email}</h3>
     <div class="fb">
       <p class="message-desc">
        ${this.text}
       </p>
     </div>
-    <button class="form-control" data-type="delete">SHOW MODAL</button>
+    <button class="form-control" data-type="show">SHOW MODAL</button>
     <button class="form-control" data-type="delete">DELETE</button>
-  </li>`}}const c={form:document.querySelector(".js-form"),container:document.querySelector(".js-messages")};c.form.addEventListener("submit",l);function l(r){r.preventDefault();const s=r.target.elements.username.value,n=r.target.elements.email.value,o=r.target.elements.message.value,e=new a(s,n,o);c.container.insertAdjacentHTML("afterbegin",e.toString())}
+  </li>`}}const a={form:document.querySelector(".js-form"),container:document.querySelector(".js-messages")};a.form.addEventListener("submit",u);function u(s){s.preventDefault();const r=s.target.elements.username.value,n=s.target.elements.email.value,o=s.target.elements.message.value,e=new m(r,n,o);a.container.insertAdjacentHTML("afterbegin",e.toString()),s.target.reset()}a.container.addEventListener("click",s=>{if(!(s.target.dataset.type==="show"))return;const o=s.target.previousElementSibling.firstElementChild.textContent.trim();l.create(`
+    <div class="modalka">
+      <p>${o}</p>
+    </div>
+  `).show()});
 //# sourceMappingURL=commonHelpers.js.map
